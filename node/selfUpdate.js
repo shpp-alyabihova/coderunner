9d2528cd71f013f1201e9a7a6bfe18c1c5559fa7
alyabihova <sunny4ka@gmail.com>
2016-04-02 01:40:25 +0300

    corrects server.js

--- node/selfUpdate.js
+++ node/selfUpdate.js
@@ -19,29 +19,23 @@ var cpOptions = {
     cwd: '/home/ubuntu/coderunner'
 };
 
-var shellExec = function (command, msg) {
+var shellExec = function (command, msg, cb) {
     cp.exec(command,  cpOptions, function(error){
         if (error) {
             log.error(msg);
-            return false;
+            cb(msg);
         }
     });
     return true;
 };
 
-function selfUpdate() {
-    if (shellExec("git pull", "SelfUpdate: fail fetch from git repo")) {
-        if (shellExec("sudo initctl stop my_coderunner", "SelfUpdate: fail stop coderunner")) {
+function selfUpdate(callback) {
+    if (shellExec("git pull", "SelfUpdate: fail fetch from git repo", callback)) {
+        if (shellExec("sudo initctl stop my_coderunner", "SelfUpdate: fail stop coderunner", callback)) {
             if (shellExec("sudo initctl start my_coderunner", "SelfUpdate: fail start coderunner")) {
-                return true;
-            } else {
-                return false;
+                callback(null, true);
             }
-        } else {
-            return false;
         }
-    } else {
-        return false;
     }
 }
 
